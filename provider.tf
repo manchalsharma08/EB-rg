2cb507afff5b4e7d4845dc9ad6f0c4ee59813174
manchal <manchalsharma08@gmail.com>
2026-04-07 22:43:11 +0530

    ok

--- provider.tf
+++ provider.tf
@@ -5,18 +5,19 @@ terraform {
       version = "4.67.0"
     }
   }
-  backenb {
+  backend {
     azurer_resource_group = "MyRGc"
     storage_account_name = "myrgst5679807578jhj"                              
     container_name       = "myrgccon"                             
-    key                  = "dev.terraform.tfstate"     
+    key                  = "dev.terraform.tfstate" 
+        
   }
 }
 
 provider "azurerm" {
-  feature{
+  features {}
     tenant_id                        = "487b2b49-d378-4a01-8f7e-b709391bd996"
     subscription_id                  = "89ce96dc-8535-4140-a60a-4ddca5655e3a"
 
-  }
+  
 }
