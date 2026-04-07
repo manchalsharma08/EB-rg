9437f32d45898ade6bd8594edc8fc06c8874f4e2
manchal <manchalsharma08@gmail.com>
2026-04-07 22:46:39 +0530

    correction done in backend

--- provider.tf
+++ provider.tf
@@ -5,19 +5,18 @@ terraform {
       version = "4.67.0"
     }
   }
-  backend {
-    azurer_resource_group = "MyRGc"
-    storage_account_name = "myrgst5679807578jhj"                              
-    container_name       = "myrgccon"                             
-    key                  = "dev.terraform.tfstate" 
-        
+
+  backend "azurerm" {
+    resource_group_name  = "MyRGc"
+    storage_account_name = "myrgst5679807578jhj"
+    container_name       = "myrgccon"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-    tenant_id                        = "487b2b49-d378-4a01-8f7e-b709391bd996"
-    subscription_id                  = "89ce96dc-8535-4140-a60a-4ddca5655e3a"
 
-  
+  tenant_id       = "487b2b49-d378-4a01-8f7e-b709391bd996"
+  subscription_id = "89ce96dc-8535-4140-a60a-4ddca5655e3a"
 }
